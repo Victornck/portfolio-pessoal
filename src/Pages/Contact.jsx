7b86adf29fae4d203a1f6a408971da8f4f6012ec
Victornck <193505785+Victornck@users.noreply.github.com>
2026-01-19 17:24:16 -0300

    feat: Font size increased

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,7 @@
 function Contact() {
   return (
     <div className="mt-16 text-center">
-      <span className="bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent font-bold text-2xl">
+      <span className="bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent font-bold text-4xl">
         CONTATO
       </span>
       <p className="text-center opacity-55">Entre em contato comigo</p>
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -23,7 +23,7 @@ const projects = [
 function Projects() {
   return (
     <div className="mt-9 text-center">
-      <span className="bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent font-bold text-2xl">
+      <span className="bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent font-bold text-4xl">
         PROJETOS
       </span>
       <p className="text-center opacity-55">
