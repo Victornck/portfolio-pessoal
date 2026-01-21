725905ce4129c48ce6f360254f823bae9c66e661
Victornck <193505785+Victornck@users.noreply.github.com>
2026-01-21 19:27:05 -0300

    style: update logo image and add it to navbar

--- src/Pages/Navbar.tsx
+++ src/Pages/Navbar.tsx
@@ -32,7 +32,8 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-6 h-full flex justify-between items-center">
 
   {/* LOGO / NOME */}
-  <div className="flex flex-col leading-tight">
+  <div className="flex space-x-2 leading-tight">
+    <img className="w-5 h-5" src="/logo.png" alt="logo" />
     <span className="bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent font-bold">
       Berlinck.dev
     </span>
