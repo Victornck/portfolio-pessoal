2220144c8841d124f7bbe2b494afa3d2b4dcbf0d
Victornck <193505785+Victornck@users.noreply.github.com>
2026-01-21 20:05:19 -0300

    feat: Add part of the certificates

--- src/Pages/Button.jsx
+++ src/Pages/Button.jsx
@@ -1,13 +1,47 @@
+import { GalleryVerticalEnd } from "lucide-react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const certificate = [];
+
 function Button() {
   return (
     <div className="flex flex-col items-center justify-center text-center px-3 pt-4 space-y-2">
       <div className="flex gap-6 pt-4">
-        <a
-          href="#contact"
-          className="bg-white text-black px-8 py-3 rounded-full font-semibold hover:opacity-90 transition inline-flex items-center justify-center"
-        >
-          Entrar em contato
-        </a>
+        <div>
+          <Dialog>
+            <DialogTrigger className="bg-white text-black px-8 py-3 rounded-full font-semibold hover:opacity-90 transition inline-flex items-center justify-center">
+              <GalleryVerticalEnd />
+              Ver certificados
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                <DialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="flex flex-wrap gap-4 justify-center mt-4">
+              {[...certificate].map((logo, index) => (
+                <img
+                  key={index}
+                  src={/certificados/}
+                  alt="Tecnologia"
+                  className="h-12"
+                />
+              ))}
+            </div>
+            </DialogContent>
+          </Dialog>
+        </div>
 
         <a
           className="border border-white px-8 py-3 rounded-full font-semibold hover:bg-gradient-to-r from-orange-400 to-purple-500 hover:text-white transition hover:border-transparent "
