dc74036267d49bc1ac1e4f7c093b13d9bd4f0c8a
Victornck <193505785+Victornck@users.noreply.github.com>
2026-01-27 15:47:41 -0300

    chore: font size to navbar

--- src/Pages/Navbar.tsx
+++ src/Pages/Navbar.tsx
@@ -4,8 +4,8 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-6 h-20 flex justify-between items-center ">
         {/* LOGO / NOME */}
         <div className="flex space-x-2 leading-tight items-center">
-          <img className="w-9 h-9" src="/logo.png" alt="logo" />
-          <span className="bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent font-bold text-2xl">
+          <img className="w-6 h-6" src="/logo.png" alt="logo" />
+          <span className="bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent font-bold text-base">
             Berlinck.dev
           </span>
         </div>
