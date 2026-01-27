d962e3671c5c196cc9261928363a25fb9326de00
Victornck <193505785+Victornck@users.noreply.github.com>
2026-01-27 15:43:32 -0300

    fix: height navbar and size logo

--- src/Pages/Navbar.tsx
+++ src/Pages/Navbar.tsx
@@ -1,15 +1,16 @@
-
 function Navbar() {
   return (
-    <div className="max-w-7xl mx-auto px-6 h-full flex justify-between items-center">
-      {/* LOGO / NOME */}
-      <div className="flex space-x-2 leading-tight">
-        <img className="w-5 h-5" src="/logo.png" alt="logo" />
-        <span className="bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent font-bold">
-          Berlinck.dev
-        </span>
+    <header className="fixed top-0 left-0 w-full z-50 bg-[#0F0F0F] shadow-sm">
+      <div className="max-w-7xl mx-auto px-6 h-20 flex justify-between items-center ">
+        {/* LOGO / NOME */}
+        <div className="flex space-x-2 leading-tight items-center">
+          <img className="w-9 h-9" src="/logo.png" alt="logo" />
+          <span className="bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent font-bold text-2xl">
+            Berlinck.dev
+          </span>
+        </div>
       </div>
-    </div>
+    </header>
   );
 }
 
